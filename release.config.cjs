cf7daa68bf28b894e89aed0befde20de103633a4
Radhaiya <kulkarnir27999@gmail.com>
2025-02-23 17:00:09 +0530

    feature: new feature

--- release.config.cjs
+++ release.config.cjs
@@ -1,3 +1,21 @@
 module.exports = {
-    branches: ["main", { name: "next", prerelease: true }]
+    branches: ["main", { name: "next", prerelease: true }],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        {
+          parserOpts: {
+            headerPattern: /^(\w+)(?:\((.*)\))?!?: (.*)$/,
+            headerCorrespondence: ["type", "scope", "subject"],
+            noteKeywords: ["BREAKING CHANGE"],
+            revertPattern: /^revert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
+            revertCorrespondence: ["header", "hash"],
+          },
+          releaseRules: [
+            { type: "feature", release: "minor" }, // Allow "feature" instead of "feat"
+          ],
+        },
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/npm",
+        "@semantic-release/github",
+      ],
 };
